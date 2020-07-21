8d25086bf325b603319b0c57f0fceb67a8d6e150
Casey Carroll <caseydean8@gmail.com>
2020-07-21 10:39:13 -0400

    Renamed unsafe lifecycles w/react-codemod

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  useParams,
 } from "react-router-dom";
 
 function App() {
--- server.js
+++ server.js
@@ -16,9 +16,35 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/reactreadinglist?retryWrites=true&w=majority");
+
+// const MongoClient = require("mongodb").MongoClient;
+// const uri =
+//   "mongodb+srv://dbCaseyDean:Cdc108mn@cluster0-adjxe.mongodb.net/reactreadinglist?retryWrites=true&w=majority";
+// const client = new MongoClient(process.env.MONGODB_URI || uri, {
+//   useNewUrlParser: true,
+// });
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://dbCaseyDean:Cdc108mng@cluster0-adjxe.mongodb.net/reactreadinglist?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+  // {
+  //   useUnifiedTopology: true,
+  // }
+);
+
+//mlab config vars
+// mongodb://heroku_hhwwqsn9:2j67oq98qrqj1cm7sn7vur7c@ds235768.mlab.com:35768/heroku_hhwwqsn9
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
